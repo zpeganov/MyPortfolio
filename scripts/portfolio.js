147fb68430b8cff523105794c38d9afa7aef7854
Zach <peganovzacharia@gmail.com>
2025-09-22 19:29:26 -0700

    Created header and added additional portfolio section functionality

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,7 +6,7 @@
 //     content.classList.toggle('expanded');
 //   });
 // });
-
+function openSection(){
 const trigger = document.querySelectorAll('.portfolio-section');
 const content = document.querySelectorAll('.expand-section');
 
@@ -16,3 +16,39 @@ trigger.forEach((triggerElement, index) => {
     content[index].classList.toggle('expanded');
   });
 });
+}
+const typedTextSpan = document.querySelector('.typed-text');
+const textArray = ['Peganov, Zacharia', 'Software Developer']
+let textArrayIndex = 0;
+let charIndex = 0;
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if(charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+document.addEventListener('DOMContentLoaded', openSection);
